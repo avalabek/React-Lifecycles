15225e2d0f0dd2f0744adef233b9db5cd50e32f3
Andreya <avalabek@yahoo.com>
2018-05-13 16:15:47 -0700

    when component mounts will log

--- lifecycle-logger/src/loggify.js
+++ lifecycle-logger/src/loggify.js
@@ -2,11 +2,26 @@ import React, {Component} from 'react';
 import styled from "styled-components";
 
 export default function loggify(Wrapped){
-  
+
   let original ={};
   //store copies of functions here
   const methodsToLog = ["componentWillMount"];
-  
+  //note you should not do this in a production environment, this is jsut to learn
+  methodsToLog.forEach((method)=> {
+    if (Wrapped.prototype[method]){
+      originals[method] = Wrapped.prototype[method]
+    
+    }
+   Wrapped.prototype[method]= function(...args){
+     let original = originals[method]
+     console.groupCollapsed(`${Wrapped.displayName} called ${method}`)
+      console.groupEnd();
+      if (original){
+        original = original.bind(this)
+        original(...args)
+      }
+    } 
+  })
 
 
   return class extends Component {
