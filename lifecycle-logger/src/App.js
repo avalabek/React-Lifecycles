83b0b3debc5c8b5860c48b7055f2d367a66c2294
Andreya <avalabek@yahoo.com>
2018-05-13 17:29:25 -0700

    stopped with componentWillRecieveProps

--- lifecycle-logger/src/App.js
+++ lifecycle-logger/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   static displayName = "App";
 
   state = {
-    data: "No Data yet!"
+    data: "No Data yet!",
+    parentPoll: "No data yet"
   }
   fetchData = () => {
     console.log("Going to fetch data!");
@@ -22,18 +23,30 @@ class App extends Component {
   }
 componentDidMount(){
   this.fetchData()
+  this.createParentPoll()
   const canvasCtx = this.refs.appCanvas.getContext('2d');
   canvasCtx.fillStyle = "lightBlue";
   canvasCtx.arc(75,75,50,0, 2 * Math.PI)
   canvasCtx.fill()
 }
+
+  createParentPoll =() => {
+    this.pollInterval = setInterval(
+      ()=> {
+        this.setState ({parentPoll: getRandomInt(1,5)})
+      },
+      1000
+    )
+  }
+
   render(){
     console.log(this.state);
-    let {showPollChild} =this.state
+    let {showPollChild, parentPoll, data} =this.state
     return (
       <div>
           Hello World
-          <h4> {this.state.data}</h4>
+          <h4> {data}</h4>
+          <h4>{parentPoll}</h4>
           <canvas
             ref={"appCanvas"}
             height={200}
@@ -50,7 +63,10 @@ componentDidMount(){
           >
           {(showPollChild) ? "Hide" : "Show"} PollChild
           </button>
-          {(showPollChild) ? <PollChild/> : null}    
+          {(showPollChild) ? (
+          <PollChild
+            parentPoll = {parentPoll}
+          /> ): null}    
       </div>    
     )
   }
@@ -82,10 +98,18 @@ class PollChild extends Component {
     //pollChild's render function
     render(){
       return(
+        <div>
         <h4>poll {this.state.poll}</h4>
+        <h4>parentPoll: {this.props.parentPoll}</h4>
+        </div>
       )
     }
 
+}
+function getRandomInt(min,max){
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random()*(max-min+1)) + min
 }
 
 App = loggify(App);
--- lifecycle-logger/src/loggify.js
+++ lifecycle-logger/src/loggify.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react';
 import styled from "styled-components";
 
+// setting state is an asynch action
+// there are many ways to set state
+
+// //es6 concise
+// this.setState({ someKey: newValue})
+// //es7 more concise
+// this.setState({someKey})
+
+//use expanded version to avoid problems with set state
+// this.setState(
+//   (prevState, props) => {
+//     //perform any actions here and ahve access to previous state
+//     return {
+//       // new object
+//     }
+//   },
+// ()=> {
+//   //my callback function
+// }
+// )
+
+
+
+
+
 export default function loggify(Wrapped){
 
   let originals ={};
   //store copies of functions here
-  const methodsToLog = ["componentWillMount", "componentDidMount", "componentWillUnmount"];
+  const methodsToLog = [
+    "componentWillMount", 
+    "componentDidMount", 
+    "componentWillUnmount",
+    "componentWillReceiveProps"
+  ];
   //note you should not do this in a production environment, this is jsut to learn
   methodsToLog.forEach((method)=> {
     if (Wrapped.prototype[method]){
@@ -15,12 +45,26 @@ export default function loggify(Wrapped){
    Wrapped.prototype[method]= function(...args){
      let original = originals[method]
      console.groupCollapsed(`${Wrapped.displayName} called ${method}`)
-      console.groupEnd();
+      
+     if (method === 'componentWillReceiveProps'){
+       console.log("nextProps", args[0])
+     }
+     
+     
+     console.groupEnd();
       if (original){
         original = original.bind(this)
         original(...args)
       }
     } 
+//do not do below in production environment for educational purposes only esp line 55
+    Wrapped.prototype.setState = function(partialState, callback){
+      console.groupCollapsed(`$Wrapped.displayName} setState`);
+      console.log('partialSTate', partialState);
+      console.log('callback', callback);
+      console.groupEnd();
+      this.updater.enqueueSetState(this,partialState, callback, 'setState')
+    }
   })
 
 
