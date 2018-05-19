4c8df9d09de6c408b8c1fbdd54d9ecabd100b077
Andreya <avalabek@yahoo.com>
2018-05-18 20:18:16 -0700

    typos

--- lifecycle-logger/src/App.js
+++ lifecycle-logger/src/App.js
@@ -1,55 +1,118 @@
-// import React, { Component } from 'react';
-// import Button from "./Button";
-// import ButtonToggle from "./ButtonToggle";
-// import Stateless from "./Stateless";
-// import StatelessButton from "./StatelessButton";
-
-// class App extends Component {
+import React, { Component } from 'react';
+import loggify from "./loggify";
 
 
-//   // state = {
-//   //   data: "No Data yet!",
-//   //   parentPoll: "No data yet"
-//   // }
-// handleClick(){
-//   let speech = "blah,blah,blah";
-//   alert(speech);
+class App extends Component {
 
-  
-// }
+  static displayName = "App";
 
-//   render() {
-//     return (
-//       <div>
-//         <h1> hello world </h1>
-//         <Stateless />
-//         <StatelessButton />
-//         <Button 
-//           onClick={this.handleClick}
-          
-//           />
-//          <ButtonToggle /> 
-//       </div>
-//     )
-//   }
-// }
-// export default App;
-import React, {Component} from "react";
-import StatelessButton from "./StatelessButton";
-import Input from "./Input";
-import Loading from "./Loading";
+  state = {
+    data: "No Data yet!",
+    parentPoll: "No data yet"
+  }
+  fetchData = () => {
+    console.log("Going to fetch data!");
+    setTimeout(
+      () => {
+        console.log("Data retrieved");
+        this.setState({
+          data: Math.random()
+        })
+      }, 1500
+    )
+  }
+componentDidMount(){
+  this.fetchData()
+  this.createParentPoll()
+  const canvasCtx = this.refs.appCanvas.getContext('2d');
+  canvasCtx.fillStyle = "lightBlue";
+  canvasCtx.arc(75,75,50,0, 2 * Math.PI)
+  canvasCtx.fill()
+}
 
-class App extends Component{
+  createParentPoll =() => {
+    this.pollInterval = setInterval(
+      ()=> {
+        this.setState ({parentPoll: getRandomInt(1,5)})
+      },
+      1000
+    )
+  }
 
-  render() {
+  render(){
+    console.log(this.state);
+    let {showPollChild, parentPoll, data} =this.state
     return (
       <div>
-        <StatelessButton />
-        <Input />
-        <Loading text="Andreya"/>
-
-      </div>  
+          Hello World
+          <h4> {data}</h4>
+          <h4>{parentPoll}</h4>
+          <canvas
+            ref={"appCanvas"}
+            height={200}
+            width={200}
+            />
+          <button
+            onClick={()=>
+            this.setState((prevState)=> {
+              return {
+                showPollChild: !prevState.showPollChild
+              }
+            })
+          }
+          >
+          {(showPollChild) ? "Hide" : "Show"} PollChild
+          </button>
+          {(showPollChild) ? (
+          <PollChild
+            parentPoll = {parentPoll}
+          /> ): null}    
+      </div>    
     )
   }
 }
+//parent component is ready to show pollchild child component
+class PollChild extends Component {
+  static displayNmae = "PollChild";
+  //another way to initialize state w/o constructor is :
+  state = {
+    poll: Math.random()
+  }
+  componentDidMount(){
+    this.pollData()
+  }
+  componentWillUnmount(){
+    clearInterval(this.pollInterval);
+  }
+  pollData = () => {
+    this.pollInterval = setInterval(
+      ()=>{
+        console.log("Poll!")
+        this.setState({
+          poll:Math.random()
+        })
+      },
+      1000
+    )
+  }
+    //pollChild's render function
+    render(){
+      return(
+        <div>
+        <h4>poll {this.state.poll}</h4>
+        <h4>parentPoll: {this.props.parentPoll}</h4>
+        </div>
+      )
+    }
+
+}
+function getRandomInt(min,max){
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random()*(max-min+1)) + min
+}
+
+App = loggify(App);
+PollChild = loggify(PollChild);
+
 export default App;
